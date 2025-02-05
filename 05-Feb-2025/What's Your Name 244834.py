847715bfcd204eed692a4b2dab96aea290c96c63
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-05 21:30:14 +0300

    Created 'What's Your Name' by A2SV Hub

---
+++ 05-Feb-2025/What's Your Name 244834.py
@@ -0,0 +1,4 @@
+# Problem: What's Your Name - https://www.hackerrank.com/challenges/whats-your-name/problem?isFullScreen=true
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
