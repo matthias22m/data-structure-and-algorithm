e1a909ed712d426535f5181848526e43c14aeb80
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-05 21:25:35 +0300

    Created 'Python Loops' by A2SV Hub

---
+++ 05-Feb-2025/Python Loops 244821.py
@@ -0,0 +1,6 @@
+# Problem: Python Loops - https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    for num in range(n):
+        print(num**2)
