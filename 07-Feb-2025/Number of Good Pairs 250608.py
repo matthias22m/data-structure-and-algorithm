e2ba326345e103aac7eb0c8752d8b701b7bf128c
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-07 21:18:41 +0300

    Created 'Number of Good Pairs' by A2SV Hub

---
+++ 07-Feb-2025/Number of Good Pairs 250608.py
@@ -0,0 +1,10 @@
+# Problem: Number of Good Pairs - https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    count+=1
+        return count
