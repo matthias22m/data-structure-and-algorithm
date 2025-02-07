114e61c6e51beb3ac7459cb07111531a972695a9
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-07 21:21:35 +0300

    Created 'Runner-up Score' by A2SV Hub

---
+++ 07-Feb-2025/Runner-up Score 250615.py
@@ -0,0 +1,13 @@
+# Problem: Runner-up Score - https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort(reverse=True)
+    mx = arr[0]
+    for score in arr:
+        if mx >score:
+            print(score)
+            break
+    else:
+        print(mx)
