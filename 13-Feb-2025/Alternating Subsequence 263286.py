712ab5a2e3a959f5cd09e90eb396ed0ead7911e6
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-13 18:32:55 +0300

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 13-Feb-2025/Alternating Subsequence 263286.py
@@ -0,0 +1,28 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+from math import inf
+
+
+for _ in range(int(input())):
+    ln = int(input())
+    ls = [int(i) for i in input().split()]
+    neg = False if ls[0] >0 else True
+    _sum = 0
+    
+    right = 0
+    while right < len(ls):
+        if neg:
+            cur = -inf
+            while right < len(ls) and ls[right] < 0:
+                cur = max(cur,ls[right])
+                right+=1
+            neg = False
+        else:
+            cur = 0
+            while right < len(ls) and ls[right] > 0:
+                cur = max(cur,ls[right])
+                right+=1
+            neg = True
+        
+        _sum+=cur
+    print(_sum)
