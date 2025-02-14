96d8b20b33962979893cc3790195ff348a9067aa
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-14 14:30:00 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 265501.py
@@ -0,0 +1,18 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+number_of_books , limit_time = [int(i) for i in input().split()]
+book_time = [int(i) for i in input().split()]
+maximum = 0
+time_track = 0
+start = 0
+
+for t in range(number_of_books):
+    time_track += book_time[t] # Add new book 
+
+    while time_track > limit_time: # if time is not enough decrement book from start
+        time_track -= book_time[start]
+        start += 1
+
+    maximum = max(maximum,t-start+1) # calculate number of books and updating maximum if it is less
+
+print(maximum)
