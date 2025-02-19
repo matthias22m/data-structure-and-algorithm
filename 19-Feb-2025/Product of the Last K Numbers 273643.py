0024171cafb795287d20070d50545679a63c1e56
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-19 15:37:58 +0300

    Created 'Product of the Last K Numbers' by A2SV Hub

---
+++ 19-Feb-2025/Product of the Last K Numbers 273643.py
@@ -0,0 +1,28 @@
+# Problem: Product of the Last K Numbers - https://leetcode.com/problems/product-of-the-last-k-numbers/description/
+
+class ProductOfNumbers:
+
+    def __init__(self):
+        self.prefix_product = [1]
+        
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix_product.clear()
+            self.prefix_product.append(1)
+            return
+
+        self.prefix_product.append(self.prefix_product[-1]*num)
+        
+    def getProduct(self, k: int) -> int:
+        if k >= len(self.prefix_product):
+            return 0
+        return self.prefix_product[-1] // self.prefix_product[-k-1]
+        
+        
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
