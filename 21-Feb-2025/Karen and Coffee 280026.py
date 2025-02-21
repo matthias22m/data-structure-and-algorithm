3cabeffbdc9e64fbbaed2e471ca0add66d9001bf
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-22 00:55:01 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 21-Feb-2025/Karen and Coffee 280026.py
@@ -0,0 +1,26 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n,k,q = [int(i) for i in input().split()]
+pref  = [0]*200002
+mx = 0
+for i in range(n):
+    start,end = [int(i) for i in input().split()]
+    
+    pref[start]+=1
+    pref[end+1]-=1
+
+for i in range(1,200001):
+    pref[i+1] += pref[i]
+
+    if pref[i] >= k:
+        pref[i] = 1
+    else: 
+        pref[i] = 0
+
+for i in range(1,200002):
+    pref[i] += pref[i-1]
+
+for i in range(q):
+    left,right = map(int,input().split())
+
+    print(pref[right]-pref[left-1])
