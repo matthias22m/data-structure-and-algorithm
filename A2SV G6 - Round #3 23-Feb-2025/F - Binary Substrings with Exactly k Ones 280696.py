dc41ce47108e2a2f5d9c1f47bf5c8e2769d4c333
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-23 09:33:56 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 23-Feb-2025/F - Binary Substrings with Exactly k Ones 280696.py
@@ -0,0 +1,24 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+n = int(input())
+st = input()
+
+def atMostK(n):
+    if n == -1:
+        return 0
+    count = 0
+    left = 0
+    ans = 0
+
+    for right in range(len(st)):
+        if st[right] == '1':
+            count += 1
+        while count > n:
+            if st[left] == '1':
+                count -= 1
+            left += 1
+        ans += right-left+1
+
+    return ans
+
+print(atMostK(n)-atMostK(n-1))
