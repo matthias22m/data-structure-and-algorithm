fb36a69d5e357b5c9cab04b89db301d68952cb1c
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-23 11:04:17 +0300

    Created 'F - Array Transformation' by A2SV Hub

---
+++ A2SV G6 - Round #2 23-Feb-2025/F - Array Transformation 280724.py
@@ -0,0 +1,22 @@
+# Problem: F - Array Transformation - https://codeforces.com/gym/586960/problem/F
+
+from collections import Counter
+from math import inf
+
+for _ in range(int(input())):
+    ln = int(input())
+    ls = [int(i) for i in input().split()]
+    cnt = Counter(ls)
+    freq = list(cnt.values())
+    unique = set(freq)
+    _min = inf
+    for f in unique:
+        count = 0
+        for f2 in freq:
+            if f > f2:
+                count += f2
+            else:
+                count += f2 - f
+        _min = min(_min,count)
+
+    print(_min)
