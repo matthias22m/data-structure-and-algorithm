a41dfb494a6e13ca29ebc9966e564bb823a1d83e
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-02-27 22:29:09 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 27-Feb-2025/D - Minimize the Distance 286421.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+from math import inf
+
+
+n = int(input())
+ls = [int(i) for i in input().split()]
+ls.sort()
+if n%2 == 0:
+    n -= 1
+print(ls[n//2])
