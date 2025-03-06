2018d16f8f1c48b24923415e7f06d09e5bb3a45e
Matthias Mulugeta <101549294+matthias22m@users.noreply.github.com>
2025-03-06 14:46:21 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 06-Mar-2025/Array Splitting 294205.py
@@ -0,0 +1,16 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+ln,k =  [int(i) for i in input().split()]
+ls =  [int(i) for i in input().split()]
+
+diff = []
+
+for i in range(1,ln):
+    diff.append(ls[i-1] - ls[i])
+    
+diff.sort()
+ans = ls[-1]-ls[0]
+
+for i in range(k-1):
+    ans+=diff[i]
+print(ans)
